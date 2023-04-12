2698df99debe8c2b21e18505740a8be499b3cb1c
alireza <comolestyrusistantaneos@gmail.com>
2023-04-12 14:06:46 +0330

    idk

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,10 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div>
+          <h1> Sign in </h1>
+          <SignInButton />
+        </div>
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
       </main>
     </>
